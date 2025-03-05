8736f98ba2c8549dced70fd669a521cc48650ecc
runqiu.bao <runqiu.bao@mujin.co.jp>
2025-03-05 13:51:14 +0900

    fix install script

--- scripts/install_genesis_in_docker.sh
+++ scripts/install_genesis_in_docker.sh
@@ -1,3 +1,7 @@
+readonly RED='\033[1;31m'
+readonly GREEN='\033[0;32m'
+readonly RESET_COLOR='\e[0m'
+
 (cd ../
 git submodule update --init --recursive)
 
@@ -5,8 +9,7 @@ git submodule update --init --recursive)
 apt update
 apt install python3-pip -y
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
-pip3 install pip-tools && pip-compile pyproject.toml
-(cd ../libs/Genesis/ && pip3 install . -i https://pypi.org/simple)
+(cd ../libs/Genesis/ && pip3 install . -i https://pypi.org/simple) && echo -e "${GREEN}installation of genesis succeeded!${RESET_COLOR}"
 pip3 install joblib==1.2.0 -i https://pypi.org/simple
 pip3 install scikit-learn==1.5.2 -i https://pypi.org/simple
 pip3 install numpy==1.26.4 -i https://pypi.org/simple
@@ -14,7 +17,7 @@ pip3 install networkx==2.8.8 -i https://pypi.org/simple
 pip3 install scikit-image==0.19.3 -i https://pypi.org/simple
 pip3 install trimesh -i https://pypi.org/simple
 mv /opt/lib/python3.9/site-packages/mpl_toolkits /opt/lib/python3.9/site-packages/__deprecated__mpl_toolkits
-echo -e "finished installing genesis."
+echo -e "${GREEN}finished installing genesis.${RESET_COLOR}"
 
 # install LuisaRenderer
 (cd ../libs/Genesis/genesis/ext/LuisaRender/build/bin/
