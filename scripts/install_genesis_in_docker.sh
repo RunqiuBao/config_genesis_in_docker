dde77dd0507448baf06e3a893599f322f1ebd855
runqiu.bao <runqiu.bao@mujin.co.jp>
2025-03-26 17:26:24 +0900

    use system cuda for pytorch

--- scripts/install_genesis_in_docker.sh
+++ scripts/install_genesis_in_docker.sh
@@ -5,10 +5,15 @@ readonly RESET_COLOR='\e[0m'
 (cd ../
 git submodule update --init --recursive)
 
-# install genesis
+# install torch
 apt update
 apt install python3-pip -y
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+pip3 install torch torchvision --no-deps
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/nccl/lib:$LD_LIBRARY_PATH
+
+# install genesis
 apt install python3-dev -y
 (cd ../libs/Genesis/ && pip3 install -r requirements.txt -i https://pypi.org/simple && pip install --prefix=/usr/local dist/genesis_world-0.2.1-cp39-cp39-linux_x86_64.whl --no-deps) && echo -e "${GREEN}installation of genesis succeeded!${RESET_COLOR}"
 mv /opt/lib/python3.9/site-packages/mpl_toolkits /opt/lib/python3.9/site-packages/__deprecated__mpl_toolkits
